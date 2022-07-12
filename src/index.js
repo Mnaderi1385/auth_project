4ad2c044713d76de384484df29267299947bd2bc
Mnaderi1385 <mo.naderi1385@gmail.com>
2022-07-11 17:31:18 -0700

    Update

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React, { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './Components/App';
 import { BrowserRouter } from 'react-router-dom';
-
+import './app.css';
 
 const root = ReactDOM.createRoot(document.querySelector('#root'));
 root.render(
